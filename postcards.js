eb8763bdb71f2331fd83bca218a4485e9508fe60
Nick Doiron <ndoiron@mapmeld.com>
2016-11-01 14:52:27 -0400

    swipe ribbon and effects

--- postcards.js
+++ postcards.js
@@ -1,7 +1,28 @@
-document.getElementById("wrapper").onclick = function() {
-  if (document.getElementById("wrapper").className === "flip") {
-    document.getElementById("wrapper").className = "noflip";    
-  } else {
-    document.getElementById("wrapper").className = "flip";
-  }
+var original = 0;
+
+var img = new Image();
+img.onload = function() {
+  document.getElementById("loading").style.display = "none";
+  document.getElementById("photo").style.backgroundImage = "url('./photo.jpg')";
 };
+img.src = './photo.jpg';
+
+function reveal() {
+  document.getElementById("ribbon").style.display = "none";
+  document.getElementById("front").style.display = "block";
+  document.getElementById("back").style.display = "block";
+}
+
+var mc = new Hammer(document.getElementById("wrapper"));
+var swiped = false;
+mc.on('swipe', function() {
+  if (swiped) {
+    return;
+  }
+  swiped = true;
+  reveal();
+  mc.on('tap', function() {
+    original += 180;
+    document.getElementById("wrapper").style.transform = 'rotateY(' + original + 'deg)';
+  });
+});
